aed3bcc32fbb952c8f6b94fb0b3c3f8c07f1c916
Alez-zavala <rzavala2320@gmail.com>
2024-07-29 16:28:25 -0800

    ..

--- Persona.java
+++ Persona.java
@@ -10,6 +10,15 @@ package modelo;
  */
 abstract class Persona {
     private String  nombres,apellidos,direccion,telefono,fecha_nacimiento;
+    
+    public Persona(){}
+    public Persona(String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
 
     public String getNombres() {
         return nombres;
@@ -50,8 +59,9 @@ abstract class Persona {
     public void setFecha_nacimiento(String fecha_nacimiento) {
         this.fecha_nacimiento = fecha_nacimiento;
     }
+
+    protected void agregar (){
+    this.nombres = nombres;
+    }
     
-    protected void agregar (){}
-    protected void modificar (){}
-    protected void eliminar (){}
 }
